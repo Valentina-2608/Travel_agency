73e600464735fa6159c1d97944176057d92fd363
Valetina-2608 <valusha2608@gmail.com>
2024-12-17 18:45:52 +0200

    Add tours_switzerland.js

---
+++ tours_switzerland.js
@@ -0,0 +1,57 @@
+let tours = [
+    {
+        "id":1,
+        "image":"img/zurich_2.jpg",
+        "caption":"Incredible Switzerland and Alsace",
+        "days":"5",
+        "moves":"no",
+        "towns":"Salzburg, Lucerne, Zurich",
+        "price":"345 Euro",
+    },
+    {
+        "id":2,
+        "image":"img/lucerne_1.jpg",
+        "caption":"Wonderful days in Switzerland",
+        "days":"5",
+        "moves":"2",
+        "towns":"Vienna, Lucerne, Zurich",
+        "price":"235 Euro",
+    },
+    {
+        "id":3,
+        "image":"img/lucerne_2.jpg",
+        "caption":"Weekend in Switzerland",
+        "days":"4",
+        "moves":"no",
+        "towns":"Krakow, Lucerne, Zurich",
+        "price":"276 Euro",
+    },
+    {
+        "id":4,
+        "image":"img/paris_2.jpeg",
+        "caption":"France and Switzerland",
+        "days":"7",
+        "moves":"no",
+        "towns":"Paris, Lucerne, Zurich",
+        "price":"385 Euro",
+    },
+    {
+        "id":5,
+        "image":"img/locarno.jpg",
+        "caption":"Italy and Switzerland",
+        "days":"5",
+        "moves":"2",
+        "towns":"Krakow, Locarno, Milan",
+        "price":"267 Euro",
+    },
+    {
+        "id":6,
+        "image":"img/zurich_3.jpg",
+        "caption":"Paris and Zurich",
+        "days":"6",
+        "moves":"no",
+        "towns":"Paris, Zurich, Prague",
+        "price":"390 Euro",
+    }
+
+]
